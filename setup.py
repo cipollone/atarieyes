bd74090f63ca0215e131b9d3c0bc1408888aeb16
Roberto Cipollone <cipollone.rt@gmail.com>
2020-03-20 21:24:24 +0100

    Prepared pipenv.

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
-import setuptools
+from setuptools import setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
+    dependency_links=[
+        "git+https://github.com/tensorforce/tensorforce.git@0.5.4#egg=tensorforce"
+    ],
+    install_requires=["opencv-python", "numpy", "gym[atari]", "tensorflow==2.1"],
     name="atarieyes",
     version="0.0.1",
     author="Roberto Cipollone",
@@ -13,9 +17,6 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/cipollone/atarieyes",
     packages=setuptools.find_packages(include=["atarieyes"]),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='~=3.6',
+    classifiers=["Programming Language :: Python :: 3",],
+    python_requires="~=3.6",
 )
