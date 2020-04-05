fb59fcf37cd430bf294be23a7f91f2ead90014d6
Roberto Cipollone <cipollone.rt@gmail.com>
2020-04-05 17:42:25 +0200

    Changed dependendes -tensorforce +keras-rl.
    + Removed tensorforce, added keras-rl. Code won't run now.

--- setup.py
+++ setup.py
@@ -7,12 +7,16 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    dependency_links=[
-        "git+https://github.com/tensorforce/tensorforce.git@0.5.4#egg=tensorforce"
+    dependency_links=[],
+    install_requires=[
+        "opencv-python",
+        "numpy",
+        "gym[atari]",
+        "tensorflow==2.1",
+        "keras-rl",
     ],
-    install_requires=["opencv-python", "numpy", "gym[atari]", "tensorflow==2.1"],
     name="atarieyes",
-    version="0.0.2",
+    version="0.0.3",
     author="Roberto Cipollone",
     author_email="cipollone.rt@gmail.com",
     description="RL on the Atari Games with feature extraction.",
