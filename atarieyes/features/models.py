Important bugfix: bug caused implicit broadcast.
+ BernoulliPair is now non-optional. I forgot to pass the correct batch,
  causing an error in sampling. I was sampling just one element, not one
  batch. Then this error was hidden by an implicit broadcast.

atarieyes/features/models.py
@@ -185,20 +185,22 @@ class BinaryRBM(Model):
     Assuming every tensor is a float.
     """
 
-    def __init__(self, n_visible, n_hidden):
+    def __init__(self, n_visible, n_hidden, batch_size):
         """Initialize.
 
         :param n_visible: number of visible units.
         :param n_hidden: number of hidden units.
+        :param batch_size: fixed size of the batch.
         """
 
         # Store
         self.n_visible = n_visible
         self.n_hidden = n_hidden
+        self.batch_size = batch_size
 
         # Two layers
         self.layers = self.BernoulliPair(
-            n_visible=n_visible, n_hidden=n_hidden)
+            n_visible=n_visible, n_hidden=n_hidden, batch_size=batch_size)
 
         # Keras model
         inputs = tf.keras.Input(shape=[n_visible], dtype=tf.float32)
@@ -284,7 +286,7 @@ class BinaryRBM(Model):
         units.
         """
 
-        def __init__(self, *, n_visible, n_hidden, batch_size=1):
+        def __init__(self, *, n_visible, n_hidden, batch_size):
             """Initialize.
 
             :param n_visible: number of visible units.
@@ -539,12 +541,15 @@ class LocalFluent(Model):
     This model is composed by a RBM (and some other parts that will be added).
     """
 
-    def __init__(self, env_name, region, n_hidden, resize_pixels=500):
+    def __init__(
+        self, env_name, region, n_hidden, batch_size, resize_pixels=500
+    ):
         """Initialize.
 
         :param env_name: a gym environment name.
         :param region: name of the selected region.
         :param n_hidden: number of hidden/output binary units.
+        :param batch_size: fixed size of the batch.
         :param resize_pixels: the input region is resized to have less than
             this number of pixels.
         """
@@ -567,7 +572,8 @@ class LocalFluent(Model):
         n_pixels = self._region_shape.num_elements()
 
         # RBM block
-        self.rbm = BinaryRBM(n_visible=n_pixels, n_hidden=n_hidden)
+        self.rbm = BinaryRBM(
+            n_visible=n_pixels, n_hidden=n_hidden, batch_size=batch_size)
 
         # Keras model
         inputs = tf.keras.Input(shape=self._frame_shape, dtype=tf.uint8)

atarieyes/features/training.py
@@ -27,6 +27,7 @@ class Trainer:
         self.init_step = args.cont if self.cont else 0
         self.learning_rate = args.learning_rate
         self.decay_rate = args.decay_rate
+        self.batch_size = args.batch_size
 
         # Dirs
         model_path, log_path = tools.prepare_directories(
@@ -44,7 +45,9 @@ class Trainer:
 
         # Model
         self.model = models.LocalFluent(
-            args.env, region="blue_right", n_hidden=args.n_hidden)
+            args.env, region="blue_right", n_hidden=args.n_hidden,
+            batch_size=args.batch_size,
+        )
 
         # Optimization
         if self.decay_rate:
@@ -65,7 +68,7 @@ class Trainer:
 
         # New run
         if not self.cont:
-            self.logger.save_graph(self.frame_shape)
+            self.logger.save_graph((self.batch_size, *self.frame_shape))
         # Restore
         else:
             self.saver.load(step)
@@ -257,7 +260,7 @@ class TensorBoardLogger:
         """Visualize the graph of the model in TensorBoard.
 
         :param input_shape: the shape of the input tensor of the model
-            (without batch).
+            (with batch).
         """
 
         # Forward pass
@@ -265,7 +268,7 @@ class TensorBoardLogger:
         def tracing_model_ops(inputs):
             return self.model.compute_all(inputs)
 
-        inputs = np.zeros((1, *input_shape), dtype=np.uint8)
+        inputs = np.zeros(input_shape, dtype=np.uint8)
 
         # Now trace
         tf.summary.trace_on(graph=True)