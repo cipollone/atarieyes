4b4154349e4f67b3109cee823502339ca0323058
Roberto Cipollone <cipollone.rt@gmail.com>
2020-05-17 17:24:15 +0200

    A more sensible organization of saved runs.
    + Models and logs are saved as:
      `runs/agent/<env_name>/models/weights_<step>.<ext>`
      `runs/features/<env_name>/models/weights_<step>.<ext>`

--- atarieyes/agent/playing.py
+++ atarieyes/agent/playing.py
@@ -23,7 +23,7 @@ class Player:
         The agent is reconstructed from a json of saved arguments.
         The weights to restore are loaded from a checkpoint saved by
         Trainer. Usually something like:
-            models/agent/<env_name>/weights_<step>.<ext>
+            runs/agent/<env_name>/models/weights_<step>.<ext>
 
         :param args: namespace of arguments; see --help.
         """
