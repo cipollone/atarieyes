df5c585b5a17f61108593127f03bbec3eaaffc50
Roberto Cipollone <cipollone.rt@gmail.com>
2020-05-22 12:54:17 +0200

    Working on temporal.py.
    + Saving work to reconsider the automaton representation.

--- atarieyes/features/temporal.py
+++ atarieyes/features/temporal.py
@@ -7,6 +7,8 @@ or constraints, of the symbols.
 from flloat.parser.ldlf import LDLfParser
 from flloat.ldlf import LDLfFormula
 from pythomata.impl.symbolic import SymbolicAutomaton
+from pythomata.simulator import AutomatonSimulator
+import tensorflow as tf
 
 from atarieyes.features import selector
 
@@ -19,16 +21,19 @@ class TemporalConstraints:
     the json file (parsing is a slow operation). These are constraints
     that these fluents must respect.
     Then, this class can be used to assess if and how much a sequence of
-    observation of those variables respects the constraints.
-    This is used to assess the goodness of a boolean function for the
-    evaluation of such fluents.
+    observations of those variables respects the constraints.
+    This is used to assess the goodness of n boolean functions in evaluating
+    the fluents.
     """
 
-    def __init__(self, env_name, fluents, logdir=None, verbose=True):
+    def __init__(
+        self, env_name, fluents, n_functions, logdir=None, verbose=True
+    ):
         """Initialize.
 
         :param env_name: a gym environment name.
         :param fluents: the list of propositional atoms that will be predicted.
+        :param n_functions: number of predictions to valuate in parallel.
         :param verbose: log the parsing step, because it may take a long time!
         """
 
@@ -67,28 +72,43 @@ class TemporalConstraints:
         self._str = constraint
         self._formula = formula
         self._automaton = automaton
+        self._n_functions = n_functions
+        self._n_fluents = len(self.fluents)
+
+        # Prepare all parallel simulators (automaton runners)
+        # TODO: how? all in python?
+        self._simulators = [
+            AutomatonSimulator(self._automaton)
+            for i in range(self._n_functions)
+        ]
 
     def observe(self, values):
-        """Observe a batch of predicted values for all fluents.
+        """Observe a list of predicted values for all fluents.
 
         This function must be called at each step of the temporal sequence
         to process the input values correctly.
 
-        :param values: a batch of values. This should be a Tensor of shape
-            (batch, n_fluents)
+        :param values: a batch of values. A prediction of values for each
+            function to evaluate. This should be a Tensor of shape
+            (n_functions, n_fluents).
         """
-        pass
+
+        # Check values shape
+        values = tf.convert_to_tensor(values)
+        assert values.shape == (self._n_functions, self._n_fluents)
+
+        # Move the automata
 
     def compute(self):
         """Signal the end of the trace and compute the metrics.
 
         Call this function once at the end of a trace (sequence of values).
-        It also computes the score on the metrics for each item in batch.
+        It also computes the score on the metrics for each of the n functions.
 
         The metrics are:
             1- consistency. Rate of observations spent on final states.
             2- sensitivity. Rate of reached final states.
 
-        :return: a list of metrics; each is a tensor of shape [batch].
+        :return: a list of metrics; each is a tensor of shape [n_functions].
         """
         pass
