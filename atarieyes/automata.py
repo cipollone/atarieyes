ebf0bbeb550b7d23a2b7de384710163aa53e3a6d
Roberto Cipollone <cipollone.rt@gmail.com>
2020-05-23 13:47:33 +0200

    Completed TfSymbolicAutomaton.
    + Completed TfSymbolicAutomaton:
      this class requires O(exp(n_atoms)) time and space during
      initialization. However, I allows a really fast look up of their arcs.
      You can run efficiently many simulations of the automaton in parallel.
    + The exponential cost is rarely an issue, because the original
      automaton it is build from usually has the same bound.

--- atarieyes/automata.py
+++ atarieyes/automata.py
@@ -1,23 +1,19 @@
-"""A bridbe between pythomata and Tensorflow.
+"""Tools for a fast simulation of automata in Tensorflow."""
 
-Pythomata is the representation I adopt. This module define tools for a more
-efficient execution in tensorflow.
-"""
-
+import math
 from pythomata.impl.symbolic import SymbolicAutomaton
 import numpy as np
 import tensorflow as tf
 
 
 class TfSymbolicAutomaton:
-    """A static representation of a SymbolicAutomaton with arrays.
+    """A static representation of a SymbolicAutomaton with tensors.
 
     This representation stores the description of a SymbolicAutomaton with
-    static arrays. It should be easier to simulate this automaton with
-    Tensorflow ops.
+    static arrays. It can be used to simulate many runs in parallel.
 
-    NOTE: this implementation is not efficient. Memory and time required are
-    exponential in len(atoms).
+    NOTE: This implementation is not efficient. Memory and time required
+    during the initialization step are exponential in len(atoms).
     """
 
     def __init__(self, automaton, atoms, verbose=True):
@@ -48,17 +44,43 @@ class TfSymbolicAutomaton:
         self.states = list(self.pythomaton.states)
         self.final_states = list(self.pythomaton._final_states)
 
-        # Transform transitions from symbolic to atomic
-        #   Storing transitions as single vectors:
-        #   [from_state, atom0_value, ..., atomN_value, to_state]
-        self.transitions = []
+        # Transform transitions from symbolic to atomic. Transitions are
+        #   ([from_state], [atom0_value, ..., atomN_value], [to_state])
+        from_states = []
+        symbols = []
+        to_states = []
         for state in self.states:
             for symbol in self._all_interpretations():
                 successor = self.pythomaton.get_successor(state, symbol)
                 array_symbol = [int(symbol[atom]) for atom in self.atoms]
-                self.transitions.append(
-                    np.concatenate(([state], array_symbol, [successor])))
-        self.transitions = np.array(self.transitions)
+
+                from_states.append(state)
+                symbols.append(array_symbol)
+                to_states.append(successor)
+
+        from_states = np.array(from_states, dtype=np.int32)
+        symbols = np.array(symbols, dtype=np.int32)
+        to_states = np.array(to_states, dtype=np.int32)
+
+        assert np.all(from_states >= 0) and np.all(to_states >= 0), \
+            "Expected states with positive indices"
+        assert np.all(0 <= symbols) and np.all(symbols <= 1), "Logic error"
+
+        # Use state_values as keys
+        n_atoms = len(self.atoms)
+        self._symbols_field = 10 ** math.ceil(math.log10(2 ** n_atoms))
+        self._powers2 = np.array(
+            [2 ** i for i in range(n_atoms)], dtype=np.int64)
+
+        # Lookup format
+        self.transitions = tf.lookup.StaticHashTable(
+            tf.lookup.KeyValueTensorInitializer(
+                self._to_keys(from_states, symbols), to_states),
+            default_value=-1,
+        )
+
+        assert self.transitions.key_dtype == tf.int64
+        assert self.transitions.value_dtype == tf.int32
 
         if verbose:
             print("> Converted")
@@ -76,57 +98,57 @@ class TfSymbolicAutomaton:
             }
             yield interpretation
 
+    def _to_keys(self, states, symbols):
+        """Convert the input symbols to keys.
 
-class TfSymbolicSimulator:
-    """Execution of symbolic automata with Tensorflow.
-
-    This class can simulate many instances of the same automaton in parallel.
-    """
-
-    def __init__(self, automaton, n_instances=1):
-        """Initialize.
-
-        :param automaton: a TfSymbolicAutomaton
-        :param n_instances: number of parallel simulations
+        :param states: a batch of current states; shape [N]
+        :param symbols: a batch of assignments for all atoms;
+            shape [N, n_atoms]
         """
 
-        # Store
-        self.automaton = automaton       # type: TfSymbolicAutomaton
-        self.n_instances = n_instances
+        states = tf.cast(states, dtype=tf.int64)
+        symbols = tf.cast(symbols, dtype=tf.int64)
 
-        # Check
-        if not isinstance(self.automaton, TfSymbolicAutomaton):
-            raise TypeError("Automaton must be a TfSymbolicAutomaton")
+        symbols_encoded = tf.einsum("ni,i->n", symbols, self._powers2)
+        all_encoded = states * self._symbols_field + symbols_encoded
 
-        # Current state for each instance
-        self.curr_states = tf.Variable(
-            np.full([self.n_instances], -1, dtype=np.int32), trainable=False)
+        return all_encoded
 
-        # Ready to go
-        self.reset()
+    @tf.function
+    def successors(self, states, symbols):
+        """Return the next state for each transition.
 
-    def reset(self):
-        """Resets all instances to the initial state.
-
-        :return: the current states
+        :param states: a batch of current states; shape [N]
+        :param symbols: a batch of assignments for all atoms;
+            shape [N, n_atoms]
+        :return: a batch of next states (always positive; -1 means logic error)
         """
 
-        self.curr_states.assign(
-            tf.broadcast_to(self.automaton.initial_state, [self.n_instances]))
+        # Lookup
+        keys = self._to_keys(states, symbols)
+        next_states = self.transitions.lookup(keys)
 
-        return self.curr_states
+        return next_states
 
-    def step(self, symbols):
-        """Move all instances.
+    def initial_states(self, n_instances):
+        """Return a batch of initial states
 
-        :param symbols: one batch of symbols of shape [n_instances, n_atoms].
-            Values can be 0 or 1.
-        :return: The current states
+        :param n_instances: batch size
+        :return: a one dimensional tesor of initial states
+        """
+        
+        return tf.broadcast_to([self.initial_state], [n_instances])
+
+    @tf.function
+    def is_final(self, states):
+        """Return which of these states are final.
+
+        :param states: a [n_instances] batch of states
         """
 
-        # Values are not checked
-        assert symbols.shape == [self.n_instances, len(self.automaton.atoms)]
-
-        # Find arcs
-        state_and_symbol = tf.concat((self.curr_states, symbols), axis=0)
-
+        # Check against each final state
+        checks = (
+            tf.expand_dims(states, -1) == tf.expand_dims(self.final_states, 0))
+        checks = tf.reduce_any(checks, axis=1)
+        return checks
+        
